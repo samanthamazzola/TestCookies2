d7644579552ffd95136120a9bc0804ce084a7344
samantha <smazzola7@gmail.com>
2018-08-22 10:47:32 -0400

    Update HomeController.cs

--- TestCookies2/Controllers/HomeController.cs
+++ TestCookies2/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -10,11 +10,18 @@ namespace TestCookies2.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string temp)
         {
             string Unit = "";
-            string Temp = ""; 
-            if (Request.Cookies["Unit"] == null) //takes http request from the web. the Cookie obj Unit doesn't exist with the value 75F 
+            string Temp = "";
+            //int Temp = 0;
+            
+
+            if (temp != null)
+            {
+                Temp = temp;
+            }
+            else if (Request.Cookies["Unit"] == null) //takes http request from the web. the Cookie obj Unit doesn't exist with the value 75F 
             {
                 //get temp in F
                 Temp = "75";
@@ -24,8 +31,12 @@ namespace TestCookies2.Controllers
                 Unit = Request.Cookies["Unit"].Value;
                 if (Unit == "C")
                 {
-                    Temp = "23";
-                } 
+                    Temp = ((Convert.ToInt32(temp) + 32) * 5 / 9).ToString();
+                }
+                else if (Unit == "F")
+                {
+                    Temp = ((Convert.ToInt32(temp) - 32) * 1.8).ToString();
+                }
                 else
                 {
                     Temp = "75";
@@ -37,7 +48,7 @@ namespace TestCookies2.Controllers
 
         }
 
-        public IActionResult ChangeUnit(string Unit) // identifying unit and passing along with the link itself
+        public IActionResult ChangeUnit(string Unit, string Temp) // identifying unit and passing along with the link itself
         {
             // 1. Get the data
             // verify first request
@@ -47,18 +58,18 @@ namespace TestCookies2.Controllers
                 //passing in name and value for the cookie
                 newCookie = new HttpCookie("Unit", "F"); //newCookie is built with default "F"
             }
+
             else //the cookie exists
             {
                 newCookie = Request.Cookies["Unit"];
             }
-            //save the data back
-            newCookie.Value = Unit; 
+            //save the data backs
+            newCookie.Value = Unit;
             newCookie.Expires = DateTime.Now.AddMinutes(1); // established cookie "set data" lasts for a min
             Response.Cookies.Add(newCookie); //sending cookie back to client
-            return RedirectToAction("Index"); //not returning view, redirects to Index; attaching to the http response 
+            return RedirectToAction("Index", new { temp = Temp }); //not returning view, redirects to Index; attaching to the http response 
 
         }
-
 
         public IActionResult About()
         {
